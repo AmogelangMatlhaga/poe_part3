d001a591a608cb11d2cc72f09408245d68e58c06
Amogelang Matlhaga <ST10355816@rcconnect.edu.za>
2025-06-26 11:56:36 +0200

    methods for assigning username and collecting it have been added

--- collectUsername.cs
+++ collectUsername.cs
@@ -1,6 +1,30 @@
 ﻿namespace poe_part3
 {
-    internal class collectUsername
+    public class collectUsername
     {
+        //global variable declaration
+        private string username = string.Empty;
+
+        //method to return the username after being collected
+        public string UsernameCollection()
+        {
+            //returning the username
+            return username;
+
+        }//end of code
+
+        //assign username collected
+        public void AssignUsername(string username) 
+        {
+            
+            //assigning the username
+            this.username = username;
+
+
+
+        } //end of code
+
+
+
     }
 }
