c33d76e6b12cad01a09b59be79374dc736a3aa65
Amogelang Matlhaga <ST10355816@rcconnect.edu.za>
2025-06-26 12:18:26 +0200

    sentiment prediction results method has been added

---
+++ SentimentPrediction.cs
@@ -0,0 +1,11 @@
+﻿namespace poe_part3
+{
+    // Sentiment prediction result class
+    public class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
